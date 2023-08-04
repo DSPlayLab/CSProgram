e008f66d9723259a5046046d35ca9191330cfe4d
heewon.kim <hewon1025@naver.com>
2023-08-04 17:31:20 +0900

    @ 사용법

--- DSPlayCS/test.cs
+++ DSPlayCS/test.cs
@@ -24,6 +24,16 @@ namespace DSPlayCS
             Console.WriteLine("[32 비트] int 최솟값: {0}" ,int.MinValue);
             Console.WriteLine("[32 비트] int 최댓값: {0}", int.MaxValue);
 
+            float f = 99.99F;
+            Console.WriteLine("{0}", f);
+
+            //@ 기호로 여러 줄 문자열 사용 -> 줄 바꿈, 빈 공백까지 문자열로 저장됨
+            string multi = @"
+                안녕하세요.
+                반갑습니다.";
+            Console.WriteLine(multi);
+
+
 
             //Console.Write("줄 바꿈 없음");
             //Console.WriteLine("줄 바꿈 포함");
