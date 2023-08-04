ab0c563becec6835bb7e3f29ab2013278851a2c5
heewon.kim <hewon1025@naver.com>
2023-08-04 16:55:30 +0900

    MinValue, MaxValue

--- DSPlayCS/test.cs
+++ DSPlayCS/test.cs
@@ -21,7 +21,10 @@ namespace DSPlayCS
             Console.WriteLine("{0},{1},{2}", a, b, c);
             const int MAX = 100;
             Console.WriteLine("최대값: {0}", MAX);
-         
+            Console.WriteLine("[32 비트] int 최솟값: {0}" ,int.MinValue);
+            Console.WriteLine("[32 비트] int 최댓값: {0}", int.MaxValue);
+
+
             //Console.Write("줄 바꿈 없음");
             //Console.WriteLine("줄 바꿈 포함");
         }
